d14f84cad29151d4ed70e67279f7be80489e6822
salima <salimaounoughi@gmail.com>
2022-05-31 11:12:37 +0200

    Initial Commit

---
+++ War-challenge/War_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_challengeApp.swift
+//  War-challenge
+//
+//  Created by Salima O. on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct War_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
